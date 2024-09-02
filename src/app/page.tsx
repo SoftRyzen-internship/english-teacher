cdd206c9afd2282811a6768a14ecbba1732f8b7f
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-09-02 22:16:13 +0300

    dell logo icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Logo from '@/../../public/assets/images/icons/logo.svg';
 
 export default function Home() {
   return (
     <main>
-      <Logo width={141} height={59} />
     </main>
   );
 }
