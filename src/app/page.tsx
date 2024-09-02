85b7243733ea1320ac5ee3023097a22664dcdcf1
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-09-02 19:06:51 +0300

    added icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Logo from '@/../../public/assets/images/icons/logo.svg';
 
 export default function Home() {
-  return <main>
-    <Logo
-      width={141}
-      height={59}
-    />
-  </main>;
+  return (
+    <main>
+      <Logo width={141} height={59} />
+    </main>
+  );
 }
-
