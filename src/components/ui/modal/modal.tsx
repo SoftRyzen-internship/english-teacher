397ca5141cedd3057699a86c863968ac6be468ab
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-09-04 00:34:16 +0300

    modal window components modal-window

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -5,38 +5,46 @@ import clsx from 'clsx';
 import { ModalProps } from './types';
 import CloseIcon from '@/../public/assets/images/icons/ico-x.svg';
 
-export const Modal = ({ children, isOpen, setIsOpen, contentClassName, buttonClassName, childContentClassName}: ModalProps) => {
+export const Modal = ({
+  children,
+  isOpen,
+  setIsOpen,
+  contentClassName,
+  buttonClassName,
+  childContentClassName,
+}: ModalProps) => {
   return (
     <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
-      <Dialog.Portal> 
-        <Dialog.Overlay className="fixed inset-0 bg-semiTransparentGray"/>
-          <Dialog.Content
-          className={clsx('absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-h-[90vh]  w-[89%] ',
-             contentClassName)}
-            // className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-h-[90vh]
-            //   w-[89%] sm:w-[420px] min-h-[434px] bg-bgButtonInactive p-6 pt-[111px] rounded-lg 
-            //   md:w-[526px] md:min-h-[378px] md:p-8 md:pt-[137px]
-            //   xl:w-[600px] xl:min-h-[390px]"
-            aria-describedby="modal-description"
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-semiTransparentGray" />
+        <Dialog.Content
+          className={clsx(
+            'absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-h-[90vh]  w-[89%] ',
+            contentClassName
+          )}
+          // className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-h-[90vh]
+          //   w-[89%] sm:w-[420px] min-h-[434px] bg-bgButtonInactive p-6 pt-[111px] rounded-lg
+          //   md:w-[526px] md:min-h-[378px] md:p-8 md:pt-[137px]
+          //   xl:w-[600px] xl:min-h-[390px]"
+          aria-describedby="modal-description"
+        >
+          <button
+            className={clsx(
+              'absolute text-black hover:text-buttonClickPink focus:text-buttonClickPink transition-all duration-300',
+              buttonClassName
+            )}
+            // className="absolute top-6 right-6 text-black hover:text-buttonClickPink focus:text-buttonClickPink transition-all duration-300
+            //   md:top-8 md:right-8 "
+            onClick={() => setIsOpen(false)}
           >
-            <button
-             className={clsx('absolute text-black hover:text-buttonClickPink focus:text-buttonClickPink transition-all duration-300',
-               buttonClassName)}
-              // className="absolute top-6 right-6 text-black hover:text-buttonClickPink focus:text-buttonClickPink transition-all duration-300
-              //   md:top-8 md:right-8 "
-              onClick={() => setIsOpen(false)}
-            >
-              <CloseIcon width={24} height={24}/>
-            </button>
-            <div className={clsx(childContentClassName)}>
-              {children}
-            </div>
+            <CloseIcon width={24} height={24} />
+          </button>
+          <div className={clsx(childContentClassName)}>{children}</div>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
   );
 };
-
 
 // 'use client';
 
@@ -48,12 +56,12 @@ export const Modal = ({ children, isOpen, setIsOpen, contentClassName, buttonCla
 // export const Modal = ({ children, isOpen, setIsOpen }: ModalProps) => {
 //   return (
 //     <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
-//       <Dialog.Portal> 
+//       <Dialog.Portal>
 //         <Dialog.Overlay className="fixed inset-0 bg-semiTransparentGray"/>
 //         {/* <div className="fixed inset-0 flex w-screen items-center justify-center  max-h-[90vh]"> */}
 //           <Dialog.Content
 //             className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-h-[90vh]
-//               w-[89%] sm:w-[420px] min-h-[434px] bg-bgButtonInactive p-6 pt-[111px] rounded-lg 
+//               w-[89%] sm:w-[420px] min-h-[434px] bg-bgButtonInactive p-6 pt-[111px] rounded-lg
 //               md:w-[526px] md:min-h-[378px] md:p-8 md:pt-[137px]
 //               xl:w-[600px] xl:min-h-[390px]"
 //             aria-describedby="modal-description"
