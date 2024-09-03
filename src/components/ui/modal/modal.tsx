c391a205698410bdbd5d79d6225c6def466c59a8
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-09-04 00:32:59 +0300

    modal window components modal-window

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -1,33 +1,76 @@
 'use client';
 
 import * as Dialog from '@radix-ui/react-dialog';
+import clsx from 'clsx';
 import { ModalProps } from './types';
 import CloseIcon from '@/../public/assets/images/icons/ico-x.svg';
 
-export const Modal = ({ children, isOpen, setIsOpen }: ModalProps) => {
+export const Modal = ({ children, isOpen, setIsOpen, contentClassName, buttonClassName, childContentClassName}: ModalProps) => {
   return (
     <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
-      <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-semiTransparentGray" />
-        <Dialog.Content
-          className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[420px] bg-bgButtonInactive p-6 rounded-lg
-          md:w-[526px] md:p-8
-          xl:w-[600px]"
-          aria-describedby="modal-description"
-        >
-          <Dialog.Title className="text-lg font-semibold">
-            Заголовок{' '}
-          </Dialog.Title>
-          <button
-            className="absolute top-6 right-6 text-black hover:text-buttonClickPink transition-all duration-300
-              md:top-8 md:right-8 "
-            onClick={() => setIsOpen(false)}
+      <Dialog.Portal> 
+        <Dialog.Overlay className="fixed inset-0 bg-semiTransparentGray"/>
+          <Dialog.Content
+          className={clsx('absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-h-[90vh]  w-[89%] ',
+             contentClassName)}
+            // className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-h-[90vh]
+            //   w-[89%] sm:w-[420px] min-h-[434px] bg-bgButtonInactive p-6 pt-[111px] rounded-lg 
+            //   md:w-[526px] md:min-h-[378px] md:p-8 md:pt-[137px]
+            //   xl:w-[600px] xl:min-h-[390px]"
+            aria-describedby="modal-description"
           >
-            <CloseIcon />
-          </button>
-          {children}
+            <button
+             className={clsx('absolute text-black hover:text-buttonClickPink focus:text-buttonClickPink transition-all duration-300',
+               buttonClassName)}
+              // className="absolute top-6 right-6 text-black hover:text-buttonClickPink focus:text-buttonClickPink transition-all duration-300
+              //   md:top-8 md:right-8 "
+              onClick={() => setIsOpen(false)}
+            >
+              <CloseIcon width={24} height={24}/>
+            </button>
+            <div className={clsx(childContentClassName)}>
+              {children}
+            </div>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
   );
 };
+
+
+// 'use client';
+
+// import * as Dialog from '@radix-ui/react-dialog';
+// import clsx from 'clsx';
+// import { ModalProps } from './types';
+// import CloseIcon from '@/../public/assets/images/icons/ico-x.svg';
+
+// export const Modal = ({ children, isOpen, setIsOpen }: ModalProps) => {
+//   return (
+//     <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
+//       <Dialog.Portal> 
+//         <Dialog.Overlay className="fixed inset-0 bg-semiTransparentGray"/>
+//         {/* <div className="fixed inset-0 flex w-screen items-center justify-center  max-h-[90vh]"> */}
+//           <Dialog.Content
+//             className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-h-[90vh]
+//               w-[89%] sm:w-[420px] min-h-[434px] bg-bgButtonInactive p-6 pt-[111px] rounded-lg 
+//               md:w-[526px] md:min-h-[378px] md:p-8 md:pt-[137px]
+//               xl:w-[600px] xl:min-h-[390px]"
+//             aria-describedby="modal-description"
+//           >
+//             <button
+//               className="absolute top-6 right-6 text-black hover:text-buttonClickPink focus:text-buttonClickPink transition-all duration-300
+//                 md:top-8 md:right-8 "
+//               onClick={() => setIsOpen(false)}
+//             >
+//               <CloseIcon width={24} height={24}/>
+//             </button>
+//             <div className="max-h-[calc(80vh-68px)] md:max-h-[calc(80vh-88px)] overflow-y-auto overflow-hidden bg-buttonFocusYellow">
+//               {children}
+//             </div>
+//         </Dialog.Content>
+//         {/* </div> */}
+//       </Dialog.Portal>
+//     </Dialog.Root>
+//   );
+// };
--- src/components/ui/modal/types.ts
+++ src/components/ui/modal/types.ts
@@ -4,4 +4,7 @@ export type ModalProps = {
   children: ReactNode;
   isOpen: boolean;
   setIsOpen: (open: boolean) => void;
+  contentClassName?: string;
+  buttonClassName?: string;
+  childContentClassName?: string;
 };
