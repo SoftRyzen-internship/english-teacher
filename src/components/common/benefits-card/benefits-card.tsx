bb108d502e60dec5ab6afe50c04b052c6ed4455f
Marynryn <murmar95@gmail.com>
2024-09-04 15:05:21 +0300

    create card/ json

---
+++ src/components/common/benefits-card/benefits-card.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const BenefitsCard = () => {
+  return (
+    <div className="bg-bgButtonInactive rounded-2xl flex flex-col gap-[18px]"></div>
+  );
+};
---
+++ src/components/common/benefits-card/types.ts
@@ -0,0 +1,6 @@
+export type BenefitsCardProps = {
+  id: string;
+  name: string;
+  title: string;
+  text: string;
+};
