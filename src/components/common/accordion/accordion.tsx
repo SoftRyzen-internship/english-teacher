03ba6c8c8344fab431ff6971cd0df74f7bb0d763
Marynryn <murmar95@gmail.com>
2024-09-04 16:47:06 +0300

    answer padding

--- src/components/common/accordion/accordion.tsx
+++ src/components/common/accordion/accordion.tsx
@@ -22,16 +22,16 @@ export const Accordion = ({ faqs }: AccordionProps) => {
           key={item.id}
           className="border border-stokeDisabled p-5 rounded-[25px] md:p-6 font-medium"
         >
-          <FAQ.Trigger className="font-montserrat w-full inline-flex justify-between items-center cursor-pointer md:gap-12 leading-[27px]">
+          <FAQ.Trigger className="font-montserrat w-full inline-flex justify-between items-start cursor-pointer md:gap-12 leading-[27px] ">
             <span className="flex-1  smOnly:max-w-[75%] text-left text-lg leading-[1.5] md:text-2xl">
               {item.question}
             </span>
 
             <Arrow
-              className={clsx(
-                'mr-0 ml-auto transition-transform duration-300',
-                { invisible: openItem === `item-${item.id}` }
-              )}
+              className={clsx('  transition-transform duration-300', {
+                'transition-transform duration-300 rotate-180':
+                  openItem === `item-${item.id}`,
+              })}
               width={25}
               height={25}
             />
@@ -39,14 +39,9 @@ export const Accordion = ({ faqs }: AccordionProps) => {
 
           <FAQ.Content className="mt-[30px] lg:mt-6">
             <FAQ.Trigger className=" w-full cursor-pointer font-comfortaa flex justify-between self-stretch  md:gap-8 items-end">
-              <span className="flex-1 smOnly:max-w-[75%] text-left leading-[1.5] text-sm md:leading-[1.4] md:text-lg">
+              <span className="flex-1 smOnly:max-w-[75%] md:pr-[73px] text-left leading-[1.5] text-sm md:leading-[1.4] md:text-lg pr">
                 {item.answer}
               </span>
-              <Arrow
-                className=" transition-transform duration-300 rotate-180 "
-                width={25}
-                height={25}
-              />
             </FAQ.Trigger>
           </FAQ.Content>
         </FAQ.Item>
