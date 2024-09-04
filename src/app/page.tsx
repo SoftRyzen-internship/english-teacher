cc0997d674797070fb618af3d57ef389560d6658
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-09-04 22:42:58 +0300

    dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,53 +6,11 @@ import { Accordion } from '@/components/common/accordion/accordion';
 import { Logo } from '@/components/ui/logo/logo';
 import { Navigation } from '@/components/ui/navigation/navigation';
 import { executeQuery } from '@datocms/cda-client';
-import { StructuredText, renderNodeRule } from 'react-datocms';
-import {
-  isHeading,
-  isParagraph,
-  isLink,
-  isList,
-  isListItem,
-  isRoot,
-} from 'datocms-structured-text-utils';
 import React from 'react';
-import { query } from '@/api/queries';
-import { QueryResult } from '@/api/types';
+import { skillsQuery } from '@/api/queries';
+import { SkillsQueryResult } from '@/api/types';
 import { Test } from '@/components/common/test/test';
-
-const renderOptions = [
-  renderNodeRule(isHeading, ({ children, key }) => {
-    return <h2 key={key}>{children}</h2>;
-  }),
-  renderNodeRule(isParagraph, ({ children, key }) => {
-    return (
-      <p key={key} className="text-base leading-6">
-        {children}
-      </p>
-    );
-  }),
-  renderNodeRule(isLink, ({ node, children, key }) => {
-    return (
-      <a key={key} href={node.url} className="text-blue-500 underline">
-        {children}
-      </a>
-    );
-  }),
-  renderNodeRule(isList, ({ node, children, key }) => {
-    return (
-      <ul key={key} className="list-disc pl-5">
-        {children}
-      </ul>
-    );
-  }),
-  renderNodeRule(isListItem, ({ children, key }) => {
-    return (
-      <li key={key} className="mb-2">
-        {children}
-      </li>
-    );
-  }),
-];
+import { ReviewsList } from '@/components/common/reviews-list/reviews-list';
 
 export default async function Home() {
   const token = process.env.DATOCMS_READONLY_TOKEN;
@@ -61,21 +19,14 @@ export default async function Home() {
     throw new Error('DATOCMS_READONLY_TOKEN is not defined');
   }
 
-  const result: QueryResult = await executeQuery(query, {
+  const result: SkillsQueryResult = await executeQuery(skillsQuery, {
     token: token,
   });
 
   return (
     <main>
       <div className="container">
-        <div>
-          <h1>{result.privacyPolicyPage.title}</h1>
-          <StructuredText
-            data={result.privacyPolicyPage.text}
-            customNodeRules={renderOptions}
-          />
-        </div>
-
+        <ReviewsList />
         <div>
           {result.teacherSkillsProfile.skillPlate.map((skill) => (
             <div key={skill.id}>
---
+++ src/sections/policy/policy.tsx
@@ -0,0 +1,92 @@
+import { StructuredText, renderNodeRule } from 'react-datocms';
+import {
+  isHeading,
+  isParagraph,
+  isLink,
+  isList,
+  isListItem,
+  isRoot,
+} from 'datocms-structured-text-utils';
+import { PolicyProps } from './types';
+import Link from 'next/link';
+
+const renderOptions = [
+  renderNodeRule(isHeading, ({ children, key }) => {
+    return (
+      <h2 key={key} className="section-subtitle mt-[30px] xl:mt-[60px] mb-5">
+        {children}
+      </h2>
+    );
+  }),
+  renderNodeRule(
+    isParagraph,
+    ({ adapter: { renderNode }, node, children, key, ancestors }) => {
+      if (isRoot(ancestors[0])) {
+        return renderNode(
+          'p',
+          {
+            key,
+            className: 'leading-[1.5] text-xs md:text-base mb-[18px] md:mb-6',
+          },
+          children
+        );
+      } else {
+        return renderNode(
+          'p',
+          {
+            key,
+            className: 'leading-[1.5] text-xs md:text-base',
+          },
+          children
+        );
+      }
+    }
+  ),
+  renderNodeRule(isLink, ({ node, children, key }) => {
+    const isEmail = node.url.includes('@');
+
+    if (isEmail) {
+      return (
+        <a key={key} href={node.url}>
+          {children}
+        </a>
+      );
+    } else {
+      return (
+        <Link key={key} href="/">
+          {children}
+        </Link>
+      );
+    }
+  }),
+  renderNodeRule(isList, ({ children, key }) => {
+    return (
+      <ul key={key} className="mb-[18px] md:mb-6">
+        {children}
+      </ul>
+    );
+  }),
+  renderNodeRule(isListItem, ({ children, key }) => {
+    return (
+      <li key={key} className="custom-list-item ml-[18px] md:ml-6">
+        {children}
+      </li>
+    );
+  }),
+];
+
+export const Policy = ({ policyData }: PolicyProps) => {
+  return (
+    <section className="pt-[29px] md:pt-[34px] xl:pt-16 pb-[50px] xl:pb-20">
+      <div className="container xl:px-[135px]">
+        <h1 className="section-title text-left mb-[30px] xl:mb-16 md:text-[48px] md:leading-[1.1]">
+          {policyData.privacyPolicyPage.title}
+        </h1>
+        <StructuredText
+          data={policyData.privacyPolicyPage.text}
+          customNodeRules={renderOptions}
+        />
+      </div>
+    </section>
+  );
+};
