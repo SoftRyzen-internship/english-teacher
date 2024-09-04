46be095eb45fb8d2fc4f3aa838980a4348ecf21a
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-09-04 12:54:24 +0300

    layout social-links

---
+++ src/components/ui/social-links/social-links.tsx
@@ -0,0 +1,16 @@
+import { SocialLinksProps } from "./types";
+
+export const SocialLinks: React.FC<SocialLinksProps> = ({ sectionName }) => {
+  return (
+    <div>
+      <h3>Соц мережі:</h3>
+      {sectionName === 'contacts' && (
+        <div className="contacts-section">
+          <p>Тг: <a href="https://t.me/yuliaenglish31" target="_blank" rel="noopener noreferrer">https://t.me/yuliaenglish31</a></p>
+          <p>Пошта: <a href="mailto:sorokau138@gmail.com">sorokau138@gmail.com</a></p>
+          <p>Інста: <a href="https://www.instagram.com/yulia_tutor.eng" target="_blank" rel="noopener noreferrer">https://www.instagram.com/yulia_tutor.eng</a></p>
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ src/components/ui/social-links/types.ts
@@ -0,0 +1,4 @@
+
+export type SocialLinksProps = {
+  sectionName: string;
+}
