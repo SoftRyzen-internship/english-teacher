b97c1781a150b5e860ae65d3ca3eaea44271acd4
bonny-art <svitlana.otenko@gmail.com>
2024-09-10 18:21:06 +0300

    fix: About section bug fix

--- src/components/common/about-info/about-info.tsx
+++ src/components/common/about-info/about-info.tsx
@@ -3,7 +3,7 @@ import Arrow from '@/../public/assets/images/icons/arrow-right.svg';
 import about from '@/data/about.json';
 
 export const AboutInfo = () => {
-  const { title, aboutList } = about;
+  const { title, aboutList, arrow } = about;
 
   return (
     <div className="xl:max-w-[493px]">
@@ -12,7 +12,7 @@ export const AboutInfo = () => {
         {aboutList.map((item) => (
           <li key={item.id} className="flex flex-row gap-3">
             <div>
-              <Arrow width={24} height={24} />
+              <Arrow width={24} height={24} aria-label={arrow.alt} />
             </div>
             <p className="font-comfortaa text-base font-semibold text-text2 md:max-w-xl">
               {item.text}
