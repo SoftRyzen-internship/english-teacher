b31716264cc99d2f26347ac496b91317db802eb6
bonny-art <svitlana.otenko@gmail.com>
2024-09-12 16:44:04 +0300

    feat: Add animation to Benefits and Reviews sections

--- src/components/common/benefits-card/benefits-card.tsx
+++ src/components/common/benefits-card/benefits-card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 import { BenefitsCardProps } from './types';
 
@@ -6,14 +6,23 @@ import Student from '@/../public/assets/images/icons/advantages/individual-lesso
 import Bell from '@/../public/assets/images/icons/advantages/bell.svg';
 import Book from '@/../public/assets/images/icons/advantages/educational-bonuses.svg';
 
-export const BenefitsCard = ({ title, name, text }: BenefitsCardProps) => {
-  return (
-    <div className="bg-bgButtonInactive rounded-2xl flex flex-col p-6 md:p-10 md:px-11 text-left gap-[18px] md:w-[511px] md:mx-auto md:gap-5 xl:w-full xl:gap-6">
-      {name === 'student' && <Student width={70} height={64} />}
-      {name === 'bell' && <Bell width={49} height={64} />}
-      {name === 'book' && <Book width={74} height={64} />}
-      <h3 className="section-subtitle xl:min-h-[72px]">{title}</h3>
-      <p className="min-h-[263px] md:min-h-[163px] xl:min-h-60">{text}</p>
-    </div>
-  );
-};
+const BenefitsCard = forwardRef<HTMLDivElement, BenefitsCardProps>(
+  ({ title, name, text }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className="bg-bgButtonInactive rounded-2xl flex flex-col p-6 md:p-10 md:px-11 text-left gap-[18px] md:w-[511px] md:mx-auto md:gap-5 xl:w-full xl:gap-6"
+      >
+        {name === 'student' && <Student width={70} height={64} />}
+        {name === 'bell' && <Bell width={49} height={64} />}
+        {name === 'book' && <Book width={74} height={64} />}
+        <h3 className="section-subtitle xl:min-h-[72px]">{title}</h3>
+        <p className="min-h-[263px] md:min-h-[163px] xl:min-h-60">{text}</p>
+      </div>
+    );
+  }
+);
+
+BenefitsCard.displayName = 'BenefitsCard';
+
+export default BenefitsCard;
--- src/components/common/reviews-list/reviews-list.tsx
+++ src/components/common/reviews-list/reviews-list.tsx
@@ -1,22 +1,64 @@
 'use client';
 
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import { Slider } from '@/components/ui/slider/slider';
-import { ReviewCard } from '../review-card/review-card';
+import ReviewCard from '../review-card/review-card';
 
 import { useDeviceType } from '@/hooks/use-device-type';
 
 import reviewsData from '@/data/reviews.json';
 
-export const ReviewsList = () => {
-  const { isDesktop } = useDeviceType();
+gsap.registerPlugin(ScrollTrigger);
 
-  const cards = reviewsData.cards.map((review) => (
-    <ReviewCard key={review.id} author={review.author} text={review.text} />
+export const ReviewsList = () => {
+  const { isDesktop, isTablet, isMobile } = useDeviceType();
+  const reviewsCardsRef = useRef<(HTMLDivElement | null)[]>([]);
+
+  useEffect(() => {
+    if (isDesktop) {
+      reviewsCardsRef.current.forEach((card, index) => {
+        if (card) {
+          gsap.fromTo(
+            card,
+            { opacity: 0, x: 100 },
+            {
+              opacity: 1,
+              x: 0,
+              duration: 0.5,
+              scrollTrigger: {
+                trigger: card,
+                start: 'top 80%',
+                end: 'bottom 20%',
+                toggleActions: 'play none none reverse',
+                once: true,
+              },
+              delay: index * 0.6,
+            }
+          );
+        }
+      });
+    }
+  }, [isDesktop]);
+
+  const cards = reviewsData.cards.map((item, index) => (
+    <ReviewCard
+      key={item.id}
+      author={item.author}
+      text={item.text}
+      ref={(el) => {
+        reviewsCardsRef.current[index] = el;
+      }}
+    />
   ));
 
-  return isDesktop ? (
-    <div className="xl:flex xl:gap-6">{cards}</div>
-  ) : (
-    <Slider section="reviews">{cards}</Slider>
-  );
+  if (isDesktop) {
+    return <div className="xl:flex xl:gap-6">{cards}</div>;
+  } else if (isTablet || isMobile) {
+    return <Slider section="reviews">{cards}</Slider>;
+  } else {
+    return null;
+  }
 };
